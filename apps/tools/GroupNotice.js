18b6142d2de6d782fedd94eae49708c8c2c255f8
岩浆 <linboyan5438081@163.com>
2024-11-21 11:50:48 +0800

    feat: 支持单独讨论机器人本身进群时的进群通知

--- apps/tools/GroupNotice.js
+++ apps/tools/GroupNotice.js
@@ -1,13 +1,31 @@
 import { karin } from 'node-karin'
 import { Config } from '#components'
 
-const JoinGroupMsg = async (e, group_id) => {
-let data = Config.Other.accept.BlackGroup
- if (!data.includes(group_id)) return await e.reply('\n欢迎加入本群୯(⁠*⁠´⁠ω⁠｀⁠*⁠)୬', { at: true })
- return false
+const JoinGroupMsg = async (e, group_id, user_id) => {
+  const data = Config.Other.accept.BlackGroup
+  if (!data.includes(group_id)) {
+    if (user_id === e.self_id) {
+      switch (Config.Other.welcome?.self.mode) {
+        case '0':
+        case 0:
+          return false
+        case '2':
+        case 2:
+          return await e.reply(Config.Other.welcome?.self.GroupMessage || '\n欢迎加入本群୯(⁠*⁠´⁠ω⁠｀⁠*⁠)୬', { at: true })
+      }
+    }
+    switch (Config.Other.welcome?.default.mode) {
+      case '0':
+      case 0:
+        return false
+      default:
+        return await e.reply(Config.Other.welcome?.default.GroupMessage || '\n欢迎加入本群୯(⁠*⁠´⁠ω⁠｀⁠*⁠)୬', { at: true })
+    }
+  }
+  return false
 }
 const ExitGroupMsg = async (e, group_id, user_id) => {
-   let data = Config.Other.accept.BlackGroup
+  const data = Config.Other.accept.BlackGroup
   if (data.includes(e.group_id)) return false
   await e.reply(`用户『${user_id}』丢下我们一个人走了`)
   return true

6e69944b8cb607dcb1be868bf9112ad971873631
岩浆 <linboyan5438081@163.com>
2024-11-22 11:20:41 +0800

    fix: 支持群组续火

--- apps/续火.js
+++ apps/续火.js
@@ -3,16 +3,19 @@ import { Config } from '#components'
 
 export const 续火 = karin.task('续火', Config.Cof.corn || '0 0 * * *', async () => {
   const bots = karin.getBotAll()
-  for (const id of Config.Cof.List || '') {
-    try {
-      const msgs = Config.Cof.msg
-      const Random = Math.floor(Math.random() * msgs.length)
-      const msg = msgs[Random]
-      const elements = [segment.text(msg)]
-      const contact = karin.contactFriend(id)
-      for (const bot of bots) {
-        karin.sendMsg(bot.self_id, contact, elements)
-      }
-    } catch (error) { }
+  const list = ['group', 'friend']
+  for (const type of list) {
+    for (const id of Config.Cof.List[type] || '') {
+      try {
+        const msgs = Config.Cof.msg
+        const Random = Math.floor(Math.random() * msgs.length)
+        const msg = msgs[Random]
+        const elements = [segment.text(msg)]
+        const contact = karin[type === 'group' ? 'contactGroup' : 'contactFriend'](id)
+        for (const bot of bots) {
+          karin.sendMsg(bot.self_id, contact, elements)
+        }
+      } catch (error) { }
+    }
   }
 }, { name: '续火' })
